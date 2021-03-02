772df67d17139742cc294942df47821b924eeb78
Evgeniy Lebedinskiy <el@lecorp.ru>
2021-03-02 23:01:14 +0300

    task 2

--- src/ru/elebedinskiy/java3/Main.java
+++ src/ru/elebedinskiy/java3/Main.java
@@ -1,5 +1,6 @@
 package ru.elebedinskiy.java3;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
@@ -7,9 +8,13 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Task #1");
-        Integer[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        swap(nums, 2, 9);
-        System.out.println(Arrays.toString(nums) + "\n");
+        Integer[] nums1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        swap(nums1, 2, 9);
+        System.out.println(Arrays.toString(nums1) + "\n");
+
+        System.out.println("Task #2");
+        Integer[] nums2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(arrayToList(nums2) + "\n");
     }
 
     // 1. Написать метод, который меняет два элемента массива местами.(массив может быть любого ссылочного типа);
@@ -17,5 +22,10 @@ public class Main {
         Object buffer = array[i];
         array[i] = array[j];
         array[j] = buffer;
+    }
+
+    // 2. Написать метод, который преобразует массив в ArrayList;
+    public static <T> ArrayList<T> arrayToList (T[] inputArray){
+        return new ArrayList<T>(Arrays.asList(inputArray));
     }
 }
