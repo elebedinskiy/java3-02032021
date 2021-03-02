c95d28a93c79a3766cf3cf82ac977aa302eb2560
Evgeniy Lebedinskiy <el@lecorp.ru>
2021-03-02 22:35:47 +0300

    task 1

--- src/ru/elebedinskiy/java3/Main.java
+++ src/ru/elebedinskiy/java3/Main.java
@@ -1,8 +1,21 @@
 package ru.elebedinskiy.java3;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("Task #1");
+        Integer[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        swap(nums, 2, 9);
+        System.out.println(Arrays.toString(nums) + "\n");
+    }
+
+    // 1. Написать метод, который меняет два элемента массива местами.(массив может быть любого ссылочного типа);
+    public static void swap (Object[] array, int i, int j){
+        Object buffer = array[i];
+        array[i] = array[j];
+        array[j] = buffer;
     }
 }
