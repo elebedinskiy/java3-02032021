1cd64e878773cc52dd6d1a3b75d898bab8dfea07
Evgeniy Lebedinskiy <el@lecorp.ru>
2021-03-03 18:18:17 +0300

    task 3 (f, g)

--- src/ru/elebedinskiy/java3/Box.java
+++ src/ru/elebedinskiy/java3/Box.java
@@ -4,27 +4,29 @@ import java.util.ArrayList;
 
 public class Box <T extends Fruit> {
 
-    private final ArrayList<T> fruits = new ArrayList<>(); // в коробке будут храниться фрукты в листе
-    private int quantity;
-    private float weight;
+    // в коробке будут храниться фрукты в листе (например, 3 шт. + 1 шт. + 10 шт. итого 13 фруктов)
+    private final ArrayList<T> fruits = new ArrayList<>();
 
-    public Box(){
-        this.weight = 0.0f; // коробку создаем пустую, с нулевым весом
-        this.quantity = 0; // без фруктов
-    }
-
-    // добавляем фрукты в коробку
+    // добавляем фрукты в коробку. например, яблок 10 штук, апельсинов 5 штук.
     public void add (T incomeFruits){
         this.fruits.add(incomeFruits);
-        this.quantity += incomeFruits.getQuantity();
-        this.weight += incomeFruits.getWeght();
     }
 
+    // посчитаем вес коробки
     public float getWeight(){
+        float weight = 0.0f;
+        for (int i = 0; i < fruits.size(); i++) {
+            weight += fruits.get(i).getWeght();
+        }
         return weight;
     }
 
+    // посчитаем кол-во фруктов в коробке
     public int getQuantity() {
+        int quantity = 0;
+        for (int i = 0; i < fruits.size(); i++) {
+            quantity += fruits.get(i).getQuantity();
+        }
         return quantity;
     }
 
@@ -34,7 +36,29 @@ public class Box <T extends Fruit> {
     true - если их веса равны, false в противном случае(коробки с яблоками
     мы можем сравнивать с коробками с апельсинами);
      */
-    public boolean compare (Box compareBox){
+
+    // <?> - означает, что коробки с яблоками можно сравнивать с коробками с апельсинами
+    public boolean compare (Box<?> compareBox){
         return Math.abs(this.getWeight() - compareBox.getWeight()) < 0.000001f;
+    }
+
+    /*
+    f. Написать метод, который позволяет пересыпать фрукты из текущей коробки
+    в другую коробку(помним про сортировку фруктов, нельзя яблоки высыпать
+    в коробку с апельсинами), соответственно в текущей коробке фруктов не остается,
+    а в другую перекидываются объекты, которые были в этой коробке;
+     */
+    public void addFruitsFromIncomeBox (Box<T> incomeBox){
+
+        // добавить в содержимое текущей коробки все элементы листа с фруктами
+        this.fruits.addAll(incomeBox.getFruits());
+
+        // обнулить кол-во фруктов в пересыпаемой коробке
+        incomeBox.getFruits().clear();
+    }
+
+    // забираем все фрукты из коробки
+    public ArrayList<T> getFruits() {
+        return fruits;
     }
 }
--- src/ru/elebedinskiy/java3/Main.java
+++ src/ru/elebedinskiy/java3/Main.java
@@ -17,7 +17,7 @@ public class Main {
         System.out.println(arrayToList(nums2) + "\n");
 
         System.out.println("Task #3");
-
+        System.out.println();
         // проверили, что коробки только для фруктов
         // Box<Integer> integerBox1 = new Box<Integer>(2);
 
@@ -35,16 +35,28 @@ public class Main {
         boxApple1.add(new Apple(2));
         boxOrange1.add(new Orange(2));
 
-        boxApple2.add(new Apple(12));
-        boxOrange2.add(new Orange(10));
+        boxApple2.add(new Apple(30));
+        boxOrange2.add(new Orange(20));
 
-        System.out.println("Коробка в яблоками | вес: " + boxApple1.getWeight() + ", кол-во: " + boxApple1.getQuantity());
-        System.out.println("Коробка в апельсинами | вес: " + boxOrange1.getWeight() + ", кол-во: " + boxOrange1.getQuantity());
+        System.out.println("Коробка Яблоки-1 | вес: " + boxApple1.getWeight() + ", кол-во: " + boxApple1.getQuantity());
+        System.out.println("Коробка Апельсины-1 | вес: " + boxOrange1.getWeight() + ", кол-во: " + boxOrange1.getQuantity());
+        System.out.println("Коробка Яблоки-2 | вес: " + boxApple2.getWeight() + ", кол-во: " + boxApple2.getQuantity());
+        System.out.println("Коробка Апельсины-2 | вес: " + boxOrange2.getWeight() + ", кол-во: " + boxOrange2.getQuantity());
+        System.out.println();
+        // сравниваем коробки по весу
+        System.out.println("Равны ли по весу коробка Яблоки-1 и Яблоки-2? " + boxApple1.compare(boxApple2)); // false
+        System.out.println("Равны ли по весу коробка Апельсины-1 и Апельсины-2? " + boxOrange1.compare(boxOrange2)); // false
+        System.out.println("Равны ли по весу коробка Яблоки-1 и Апельсины-1? " + boxApple1.compare(boxOrange1)); // false
+        System.out.println("Равны ли по весу коробка Апельсины-2 и Яблоки-2? " + boxOrange2.compare(boxApple2)); // true
+        System.out.println();
 
-        System.out.println(boxApple1.compare(boxApple2)); // true
-        System.out.println(boxOrange1.compare(boxOrange2)); // false
-        // ...
-        System.out.println("\n");
+        boxApple2.addFruitsFromIncomeBox(boxApple1);
+        boxOrange2.addFruitsFromIncomeBox(boxOrange1);
+        // boxOrange2.addFruitsFromIncomeBox(boxApple2); // проверка, что в коробку с апельсинами нельзя пересыпать яблоки
+        System.out.println("Коробка Яблоки-1 | вес: " + boxApple1.getWeight() + ", кол-во: " + boxApple1.getQuantity());
+        System.out.println("Коробка Апельсины-1 | вес: " + boxOrange1.getWeight() + ", кол-во: " + boxOrange1.getQuantity());
+        System.out.println("Коробка Яблоки-2 | вес: " + boxApple2.getWeight() + ", кол-во: " + boxApple2.getQuantity());
+        System.out.println("Коробка Апельсины-2 | вес: " + boxOrange2.getWeight() + ", кол-во: " + boxOrange2.getQuantity());
     }
 
     // 1. Написать метод, который меняет два элемента массива местами.(массив может быть любого ссылочного типа);
